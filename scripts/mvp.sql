31542f86e348af3014d99f146b13e2085b0f1ec1
asialarkin <asiailarkin@gmail.com>
2023-10-12 20:26:39 -0500

    some answers

--- scripts/mvp.sql
+++ scripts/mvp.sql
@@ -1,18 +1,18 @@
 --- 1. 
    -- a. Which prescriber had the highest total number of claims (totaled over all drugs)? Report the npi and the total number of claims.
     
-	SELECT npi, SUM(total_claim_count) AS claim_count_sum
+	SELECT npi, nppes_provider_first_name,nppes_provider_last_org_name, SUM(total_claim_count) AS claim_count_sum
 	FROM prescriber
 	INNER JOIN prescription
 	USING (npi)
-	GROUP BY npi
+	GROUP BY npi,nppes_provider_first_name,nppes_provider_last_org_name
 	ORDER BY claim_count_sum DESC
 	LIMIT 5;
 	
 	
    -- b. Repeat the above, but this time report the nppes_provider_first_name, nppes_provider_last_org_name,  specialty_description, and the total number of claims.
 
-	SELECT npi, nppes_provider_first_name, nppes_provider_last_org_name,  specialty_description, SUM(total_claim_count) AS claim_count_sum
+	SELECT npi, nppes_provider_first_name, nppes_provider_last_org_name, specialty_description, SUM(total_claim_count) AS claim_count_sum
 	FROM prescriber
 	INNER JOIN prescription
 	USING (npi)
@@ -20,20 +20,60 @@
 	ORDER BY claim_count_sum DESC
 	LIMIT 5;
 
-
 --- 2. 
   --  a. Which specialty had the most total number of claims (totaled over all drugs)?
 
-   -- b. Which specialty had the most total number of claims for opioids?
+   SELECT specialty_description , SUM(total_claim_count) AS sum_claim_count
+   FROM prescriber
+   INNER JOIN prescription
+   USING (npi)
+   GROUP BY specialty_description
+   ORDER BY sum_claim_count DESC
+   LIMIT 10;
 
-  --  c. **Challenge Question:** Are there any specialties that appear in the prescriber table that have no associated prescriptions in the prescription table?
+   -- b. Which specialty had the most total number of claims for opioids?
+   
+   SELECT specialty_description , SUM(total_claim_count) AS sum_claim_count
+   FROM prescriber as p1
+   INNER JOIN prescription as p2
+   USING (npi)
+   INNER JOIN drug
+   USING (drug_name)
+   WHERE opioid_drug_flag='Y'
+   GROUP BY specialty_description
+   ORDER BY sum_claim_count DESC
+   LIMIT 10;
+
+
+ --  c. **Challenge Question:** Are there any specialties that appear in the prescriber table that have no associated prescriptions in the prescription table?
+  
+  
 
   --  d. **Difficult Bonus:** *Do not attempt until you have solved all other problems!* For each specialty, report the percentage of total claims by that specialty which are for opioids. Which specialties have a high percentage of opioids?
+
+
 
 --- 3. 
    -- a. Which drug (generic_name) had the highest total drug cost?
 
+SELECT generic_name, SUM(total_drug_cost) AS daily_cost
+FROM prescription
+LEFT JOIN drug
+	USING (drug_name)
+GROUP BY generic_name
+ORDER BY daily_cost DESC
+LIMIT 1;
+
   --  b. Which drug (generic_name) has the hightest total cost per day? **Bonus: Round your cost per day column to 2 decimal places. Google ROUND to see how this works.**
+
+SELECT generic_name, 
+	ROUND(SUM(total_drug_cost)/SUM(total_day_supply),2) AS daily_cost
+FROM prescription
+LEFT JOIN drug
+	USING (drug_name)
+GROUP BY generic_name
+ORDER BY daily_cost DESC
+LIMIT 1;
 
 --- 4. 
   --  a. For each drug in the drug table, return the drug name and then a column named 'drug_type' which says 'opioid' for drugs which have opioid_drug_flag = 'Y', says 'antibiotic' for those drugs which have antibiotic_drug_flag = 'Y', and says 'neither' for all other drugs.
